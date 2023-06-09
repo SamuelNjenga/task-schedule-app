0ade3bd3ae1fbb512f68c4d59eb83581238779c2
Samuel Njenga <snjenga704@gmail.com>
2023-06-09 11:55:23 +0300

    Style: modify page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Scheduled Tasks</h1>
+        <Link href="/new">New</Link>
+      </header>
+      <ul></ul>
+    </>
+  );
 }
