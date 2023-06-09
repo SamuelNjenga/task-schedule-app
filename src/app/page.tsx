e1c905c6157961c38b6a7b7a83d0eae51d91bfd6
Samuel Njenga <snjenga704@gmail.com>
2023-06-09 12:07:12 +0300

    Style: modify Link in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Scheduled Tasks</h1>
-        <Link href="/new">New</Link>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
       </header>
       <ul></ul>
     </>
