e9ccb05d801d62e4e68f14d8c415ac4f85a2ddce
Samuel Njenga <snjenga704@gmail.com>
2023-06-14 11:16:02 +0300

    Style: style Homepage and Learnmore buttons

--- src/app/users/registration/page.tsx
+++ src/app/users/registration/page.tsx
@@ -4,15 +4,56 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import * as React from "react";
+
+const HomepageButton = styled(Button)({
+  marginRight: "12px",
+  backgroundColor: "#0069d9",
+  "&:hover": {
+    borderColor: "#0062cc",
+    boxShadow: "none",
+  },
+});
+
+const LearnmoreButton = styled(Button)({
+  border: "1px solid",
+  backgroundColor: "#0063cc !important",
+  borderColor: "#0063cc !important",
+  fontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    '"Segoe UI"',
+    "Roboto",
+    '"Helvetica Neue"',
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(","),
+  "&:hover": {
+    backgroundColor: "#0069d9 !important",
+    borderColor: "#0062cc",
+    boxShadow: "none",
+  },
+  "&:active": {
+    boxShadow: "none",
+    backgroundColor: "#0062cc",
+    borderColor: "#005cbf",
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+});
 
 export default function Registration() {
   return (
     <>
       <header className="text-center">New User Created</header>
-      <div className="pl-14 pt-20 flex items-center justify-center">
+      <div className="pl-14 pt-10 flex items-center justify-center">
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             component="img"
@@ -31,9 +72,13 @@ export default function Registration() {
           </CardContent>
           <CardActions>
             <Link href="">
-              <Button size="small">Home Page</Button>
+              <HomepageButton size="small" variant="outlined" color="primary">
+                Home Page
+              </HomepageButton>
             </Link>
-            <Button size="small">Learn More</Button>
+            <LearnmoreButton size="small" variant="contained" color="secondary">
+              Learn More
+            </LearnmoreButton>
           </CardActions>
         </Card>
       </div>
