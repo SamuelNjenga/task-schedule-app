11775ed343fa3d45affc425bbfe3357c735b8dee
Samuel Njenga <snjenga704@gmail.com>
2023-06-16 23:01:13 +0300

    Feat: add date-fns library to ClockTime component

--- src/components/ClockTime.tsx
+++ src/components/ClockTime.tsx
@@ -1,11 +1,13 @@
+import { format } from "date-fns";
 import React from "react";
 
 const ClockTime = () => {
   return (
     <div>
       <h4 className="font-sans text-lg font-medium text-sky-400/75 underline decoration-pink-500/30">
-        The Current Time
+        The Current Time{" "}
       </h4>
+      {format(new Date(new Date()), "h 'o''clock' MM/dd/yyyy")}
     </div>
   );
 };
