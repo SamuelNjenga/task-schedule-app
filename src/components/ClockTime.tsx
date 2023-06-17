ade484fd84254da88c2347a96898261950e2f89a
Samuel Njenga <snjenga704@gmail.com>
2023-06-17 19:47:53 +0300

    Feat: add moment package for current time

--- src/components/ClockTime.tsx
+++ src/components/ClockTime.tsx
@@ -1,4 +1,5 @@
-import { format } from "date-fns";
+// import { format } from "date-fns";
+import moment from "moment";
 import React from "react";
 
 const ClockTime = () => {
@@ -7,7 +8,8 @@ const ClockTime = () => {
       <h4 className="font-sans text-lg font-medium text-sky-400/75 underline decoration-pink-500/30">
         The Current Time{" "}
       </h4>
-      {format(new Date(new Date()), "h 'o''clock' MM/dd/yyyy")}
+      {/* {format(new Date(new Date()), "h 'o''clock' MM/dd/yyyy")} */}
+      {moment(new Date()).format("MMMM Do YYYY, h:mm:ss a")}
     </div>
   );
 };
