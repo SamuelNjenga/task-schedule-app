0f1031d2e36963223ed451ecde13810d7199d90b
Samuel Njenga <snjenga704@gmail.com>
2023-06-20 20:02:37 +0300

    Feat: add ImageItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,6 +53,12 @@ export default async function Home() {
         >
           Tweets
         </Link>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/pics"
+        >
+          Images
+        </Link>
       </header>
       <h3>Tasks</h3>
       <ul className="pl-4">
---
+++ src/app/pics/page.tsx
@@ -0,0 +1,12 @@
+import ImageItem from "@/components/ImageItem";
+import React from "react";
+
+const Pics = () => {
+  return (
+    <div>
+      <ImageItem />
+    </div>
+  );
+};
+
+export default Pics;
