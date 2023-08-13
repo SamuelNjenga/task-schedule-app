fa7b6f477a4975d694c6ca1db185a2e7a827b4aa
Samuel Njenga <snjenga704@gmail.com>
2023-08-13 12:18:42 +0300

    feat: add TwitterAnimation.tsx

--- src/app/tweets/page.tsx
+++ src/app/tweets/page.tsx
@@ -1,4 +1,5 @@
 import TweetItem from "@/components/TweetItem";
+import TwitterAnimation from "@/components/TwitterAnimation";
 import React from "react";
 
 const page = () => {
@@ -6,6 +7,7 @@ const page = () => {
     <div>
       <header className="mb-4">
         <h1 className="text-2xl">Tweets Page</h1>
+        <TwitterAnimation />
         <TweetItem />
       </header>
     </div>
